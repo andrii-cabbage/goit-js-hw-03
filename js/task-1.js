28772daeb7d15eb0bc92989d03b3c19443f8c0f6
andrii-cabbage <kapysta377@gmail.com>
2024-03-24 15:47:34 +0200

    hw

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function slugify(title) {
+    title = title.toLowerCase();
+    
+    title = title.replace(/\s+/g, '-');
+    
+    return title;
+}
+
+
+console.log(slugify("Arrays for begginers")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
